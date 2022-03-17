ac28a2272c3e18ec36d0417e92fcc09013290926
Michael Law <mikelaw86@hotmail.com>
2022-03-17 18:14:26 -0400

    Add function to get ISS fly over times

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
     if (error) {
@@ -17,4 +18,15 @@ fetchCoordsByIP('24.141.241.185', (error, coords) => {
     }
 
     console.log('It worked! Returned Coordinates:' , coords);
+});
+
+const coordExample = { latitude: 43.6514, longitude: -79.9143 };
+
+fetchISSFlyOverTimes(coordExample, (error, passes) => {
+    if (error) {
+        console.log("It didn't work!" , error);
+        return;
+    }
+
+    console.log('It worked! Returned Fly Over Times:', passes);
 });
--- iss.js
+++ iss.js
@@ -28,8 +28,8 @@ const fetchCoordsByIP = function(ip, callback) {
             return;
         }
         if (response.statusCode !== 200) {
-          callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
-          return;
+            callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+            return;
         }
         const { latitude, longitude } = JSON.parse(body);
 
@@ -37,7 +37,28 @@ const fetchCoordsByIP = function(ip, callback) {
     });
 }
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+    const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+    request(url, (error, response, body) => {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+
+        if (response.statusCode !== 200) {
+            callback(Error(`Status Code ${response.statusCode} when fetching Fly Over Times with Coordinates: ${body}`), null);
+            return;
+        }
+
+        const passOver = JSON.parse(body).response;
+
+        callback(null, passOver);
+    });
+}
+
 module.exports = {
     fetchMyIP,
-    fetchCoordsByIP
+    fetchCoordsByIP,
+    fetchISSFlyOverTimes
 };
